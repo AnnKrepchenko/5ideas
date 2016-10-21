a0c34e2b8f9de6eafab9896bece8876050ee6bad
AnnKrepchenko <anna.krepchenko@gmail.com>
2016-10-21 16:24:42 +0300

    IdeaAdapter

--- app/src/main/java/com/krepchenko/a5ideas/ui/adapters/IdeaAdapter.kt
+++ app/src/main/java/com/krepchenko/a5ideas/ui/adapters/IdeaAdapter.kt
@@ -7,24 +7,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.krepchenko.a5ideas.R
+import kotlinx.android.synthetic.main.item_card_idea
 
 /**
  * Created by ann on 10/20/16.
  */
-class IdeaAdapter(context: Context?, cursor: Cursor?) : CursorRecyclerViewAdapter<IdeaAdapter.ViewHolder>(context, cursor) {
+class IdeaAdapter : CursorRecyclerViewAdapter<IdeaAdapter.ViewHolder> {
 
-    private var onRecyclerItemClick: OnRecyclerViewItemClick = null
+    private var onRecyclerItemClick: OnRecyclerViewItemClick? = null
+    private var context : Context? = null
+
+    constructor(context: Context?, cursor: Cursor?, onRecyclerItemClick: OnRecyclerViewItemClick?) : super(context,cursor) {
+        this.onRecyclerItemClick = onRecyclerItemClick
+        this.context = context
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
-        var view = LayoutInflater.from(parent?.context).inflate(R.layout.item_card_idea,parent,false)
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_card_idea,parent,false)
         return ViewHolder(view,onRecyclerItemClick)
     }
 
     override fun onBindViewHolder(viewHolder: ViewHolder?, cursor: Cursor?) {
     }
 
-    class ViewHolder(itemView: View?, onRecyclerItemClick: OnRecyclerViewItemClick) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+    override fun onBindViewHolder(viewHolder: ViewHolder?, position: Int) {
+        super.onBindViewHolder(viewHolder, position)
+    }
+
+    class ViewHolder(itemView: View?, onRecyclerItemClick: OnRecyclerViewItemClick?) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+
+        val itemIdeasName = itemView.itemIdeasName
+        val itemIdeasDesc = itemView.itemIdeasDesc
+        var onRecyclerItemClick: OnRecyclerViewItemClick? = null
+
+        constructor(itemView: View, onRecyclerItemClick: OnRecyclerViewItemClick) : super(itemView){
+
+            this.onRecyclerItemClick = onRecyclerItemClick
+            itemView!!.setOnClickListener(onRecyclerItemClick)
+        }
+
         override fun onClick(v: View?) {
+            onRecyclerItemClick.onItemClick(adapterPosition,itemView)
         }
 
     }
