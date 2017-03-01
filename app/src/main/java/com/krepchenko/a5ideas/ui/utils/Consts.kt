381600dbf8c4431b98e6dcaa7fdc58cf12e8454f
AnnKrepchenko <anna.krepchenko@gmail.com>
2017-03-01 20:59:28 +0200

    edit + pick image

--- app/src/main/java/com/krepchenko/a5ideas/ui/utils/Consts.kt
+++ app/src/main/java/com/krepchenko/a5ideas/ui/utils/Consts.kt
@@ -3,10 +3,8 @@ package com.krepchenko.a5ideas.ui.utils
 /**
  * Created by krepc on 27.02.2017.
  */
-class Consts {
-    companion object {
+object Consts {
         val EXTRA_ID = "id"
         val EXTRA_NAME = "name"
         val EXTRA_EDIT = "edit"
-    }
 }
